9269397111181247424350a329b24fe6deb28adc
Viktor Szépe <viktor@szepe.net>
2018-02-23 08:52:14 +0100

    Improve code readability

--- domReady.js
+++ domReady.js
@@ -1,10 +1,13 @@
-(function(){
+(function () {
   'use strict';
 
-  module.exports = new Promise(function(resolve, reject){
-    function retest(){/in/.test(document.readyState) ? setTimeout(retest,9) : resolve()}
+  module.exports = new Promise(function (resolve, reject) {
+    function retest() {
+      /in/.test(document.readyState) ? setTimeout(retest, 9) : resolve();
+    }
+
     if (/in/.test(document.readyState))
-      document.addEventListener ? document.addEventListener('DOMContentLoaded',resolve) : restest();
+      document.addEventListener ? document.addEventListener('DOMContentLoaded', resolve) : restest();
     else
       resolve();
   });
